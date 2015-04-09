668bcced72bbf6c28254c0ee64f8bb9db910fb42
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-04-09 14:05:33 +0200

    Add management of manager (ahah) in Museum Tests #6

--- AderFber-MuseumToulouse/test/unit/aderfber/museumtoulouse/MuseumSpec.groovy
+++ AderFber-MuseumToulouse/test/unit/aderfber/museumtoulouse/MuseumSpec.groovy
@@ -12,7 +12,7 @@ class MuseumSpec extends Specification {
         given: "an initialize museum"
         Museum museum = new Museum(name: pname, openingHours: popeninghours, phone: pphone,
                 subwayAccess: psubwayAccess,
-                busAccess: pbusAccess, address: Mock(Address))
+                busAccess: pbusAccess, address: Mock(Address), manager: Mock(Manager))
 
         expect: "Museum is valid"
         museum.validate()
@@ -30,21 +30,22 @@ class MuseumSpec extends Specification {
         "MUSEE DE L'HISTOIRE DE LA MEDECINE DE TOULOUSE"            |   "Ouvert du mardi au samedi de 13h à 19hfermé les dimanches"         |   "05 61 77 82 72"    |   null            |   null
     }
 
-    void "test an unvalid museum"(pname, popeninghours, pphone, psubwayAccess, pbusAccess, paddress) {
+    void "test an unvalid museum"(pname, popeninghours, pphone, psubwayAccess, pbusAccess, paddress, pManager) {
         given: "an initialize museum"
         Museum museum = new Museum(name: pname, openingHours: popeninghours, phone: pphone,
                 subwayAccess: psubwayAccess,
-                busAccess: pbusAccess, address: paddress)
+                busAccess: pbusAccess, address: paddress, manager: pManager)
 
         expect: "Museum is valid"
         !museum.validate()
 
         where:
-        pname                                                       |       popeninghours                                                   |       pphone          | psubwayAccess     |       pbusAccess  | paddress
-        ""                                                          |   "Ouvert tous les jours de 9h à 19h."                                |   "05 61 36 81 12"    |   "Roseraie (A)"  |   "36, 38"        |   Mock(Address)
-        null                                                        |   "Tous les jours : 10h - 18h /  nocturne le mercredi jusqu'à 21h."   |   ""                  |   "Esquirol (A)"  |   "2, 10, 24"     |   Mock(Address)
-        "MUSEE PAUL-DUPUY - ARTS GRAPHIQUES ET ARTS DECORATIFS"     |   ""                                                                  |   null                |   "Capitole (A)"  |   "2"             |   Mock(Address)
-        "MUSEE DE L'HISTOIRE DE LA MEDECINE DE TOULOUSE"            |   null                                                                |   "05 61 77 82 72"    |   ""              |   "42"            |   Mock(Address)
-        "MUSEE DE L'HISTOIRE DE LA MEDECINE DE TOULOUSE"            |   "Ouvert du mardi au samedi de 13h à 19hfermé les dimanches"         |   "05 61 77 82 72"    |   null            |   "10"            |   null
+        pname                                                       |       popeninghours                                                   |       pphone          | psubwayAccess     |       pbusAccess  | paddress          | pManager
+        ""                                                          |   "Ouvert tous les jours de 9h à 19h."                                |   "05 61 36 81 12"    |   "Roseraie (A)"  |   "36, 38"        |   Mock(Address)   |   Mock(Manager)
+        null                                                        |   "Tous les jours : 10h - 18h /  nocturne le mercredi jusqu'à 21h."   |   ""                  |   "Esquirol (A)"  |   "2, 10, 24"     |   Mock(Address)   |   Mock(Manager)
+        "MUSEE PAUL-DUPUY - ARTS GRAPHIQUES ET ARTS DECORATIFS"     |   ""                                                                  |   null                |   "Capitole (A)"  |   "2"             |   Mock(Address)   |   Mock(Manager)
+        "MUSEE DE L'HISTOIRE DE LA MEDECINE DE TOULOUSE"            |   null                                                                |   "05 61 77 82 72"    |   ""              |   "42"            |   Mock(Address)   |   Mock(Manager)
+        "MUSEE DE L'HISTOIRE DE LA MEDECINE DE TOULOUSE"            |   "Ouvert du mardi au samedi de 13h à 19hfermé les dimanches"         |   "05 61 77 82 72"    |   null            |   "10"            |   null            |   Mock(Manager)
+        "MUSEE DE L'HISTOIRE DE LA MEDECINE DE TOULOUSE"            |   "Ouvert du mardi au samedi de 13h à 19hfermé les dimanches"         |   "05 61 77 82 72"    |   null            |   "10"            |   Mock(Address)   |   null
     }
 }
