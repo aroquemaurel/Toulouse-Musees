d9f4216932b1af4ccb1a0b4d55f9a909a0a53d92
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-04-16 00:13:13 +0200

    Create Csv Service

---
+++ AderFber-MuseumToulouse/grails-app/services/aderfber/museumtoulouse/TestsCsvService.groovy
@@ -0,0 +1,33 @@
+package aderfber.museumtoulouse
+
+import grails.transaction.Transactional
+
+@Transactional
+class TestsCsvService {
+    MuseumService museumService
+
+    def parseCsv() {
+        File csv = new File("grails-app/conf/Museums.csv");
+        Manager manager
+        Address address
+        Museum museum
+
+        csv.splitEachLine(';') { row ->
+            if (row[0] != "EQ_NOM_EQUIPEMENT") {
+                manager = Manager.findByName(row[1]) ?: new Manager(name: row[1])
+
+                address = new Address(number: row[7], street: row[8], postalCode: row[9], city: row[10])
+                address = Address.find(address) ?: address
+
+                museum = Museum.findByName(row[0]) ?: new Museum(
+                        name: row[0],
+                        openingHours: row[2],
+                        phone: row[4],
+                        subwayAccess: row[5],
+                        busAccess: row[6])
+
+                museumService.insertOrUpdateMuseum(museum, manager, address)
+            }
+        }
+    }
+}
